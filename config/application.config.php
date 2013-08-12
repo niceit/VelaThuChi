ccd9e8846b7f6a51756da2927e017f23893c4498
minhtrius <minhtrius@gmail.com>
2013-08-12 15:12:02 +0700

    Config Enable module Customer
    Config Enable module Customer

--- config/application.config.php
+++ config/application.config.php
@@ -1,7 +1,7 @@
 <?php
 return array(
     'modules' => array(
-        'Application','Home',
+        'Application','Home','Customer'
     ),
     'module_listener_options' => array(
         'config_glob_paths'    => array(
